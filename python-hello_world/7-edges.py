661f65a6826301c795d7a936ae1f4631dbca3eae
akotet27 <a.demise@alueducation.com>
2026-02-25 07:28:57 -0800

    added the script

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 word = "Holberton"
 word_first_3 = "Hol"
-word_last_2 = "ho"
+word_last_2 = "on"
 middle_word = "olberto"
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
