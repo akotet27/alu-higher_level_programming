6da43c416ab8f0058f47afb51dd4b5bf10fdb323
akotet27 <a.demise@alueducation.com>
2026-03-07 02:15:11 -0800

    fixed errors in the script

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, new_element):
-    if idx < 0 or idx > len(my_list):
-        return None
-    my_list[idx] = new_element
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    my_list[idx] = element
     return my_list
-idx = 0
-new_element = 7
-
-my_list = [1, 2, 3, 4, 5]
-new_list = replace_in_list(my_list, idx, new_element)
-print(new_list)
