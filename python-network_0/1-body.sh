c3ddba1f829eae61616d38859904fd1edcd0a2c7
akotet27 <a.demise@alueducation.com>
2026-04-03 11:51:20 +0200

    Python network

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL and dispays the body of the response
-curl -sLG "$1"
+# takes in a url and displays its response body size
+curl -sI "$1" | grep "Content-Length" | awk '{print $2}'
